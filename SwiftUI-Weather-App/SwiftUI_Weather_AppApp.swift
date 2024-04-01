a8015e6f70810c4f24d16c850f0fe7d4f48df057
Steven Ferdianto <steven.ferdianto0110@gmail.com>
2024-04-01 21:57:00 +0700

    Initial Commit

---
+++ SwiftUI-Weather-App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI-Weather-App
+//
+//  Created by steven ferdianto on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
